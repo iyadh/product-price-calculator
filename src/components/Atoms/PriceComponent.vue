96dc231c0265c7af7aa8b72777e08927d9765de5
Mohamed Gallah <mohamed.gallah@gmail.com>
2022-09-16 19:27:30 +0200

    ✨ Edit and delete price component

--- src/components/Atoms/PriceComponent.vue
+++ src/components/Atoms/PriceComponent.vue
@@ -1,60 +1,72 @@
 <template>
   <div
-    class="relative flex justify-between gap-4 px-8 py-4"
+    class="relative flex justify-between px-8 py-4"
     @mouseleave="hover = false"
     @mouseover="hover = true"
   >
     <label
       :for="'price-' + props.args.id"
       class="w-2/4 block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2 cursor-pointer"
-      @click="filterValue"
+      @click="editPrice"
       >{{ props.args.label }}</label
     >
-    <button v-show="hover" class="absolute top-6 right-80">
-      <PencilSquareIcon
-        class="h-5 fill-slate-600 transition hover:fill-slate-400 hover:rotate-[-4deg]"
-      />
-    </button>
     <span
       v-if="!edit"
-      class="w-1/4 block rounded-md text-sm leading-5 py-2 px-3 mr-10 border-2 border-slate-300 shadow-sm"
+      class="w-1/4 block rounded-md text-sm leading-5 py-2 px-3 mr-14 border-2 border-slate-300 shadow-sm"
       >{{ computedPrice }}</span
     >
     <input
       v-if="edit"
       :id="'price-' + props.args.id"
+      ref="input"
       v-model="price"
-      class="w-1/4 block rounded-md text-sm leading-5 py-2 px-3 mr-10 border-2 border-slate-300 shadow-sm focus:outline-none focus:ring focus:border-indigo-500 focus:ring-indigo-200"
+      class="w-1/4 block rounded-md text-sm leading-5 py-2 px-3 mr-14 border-2 border-slate-300 shadow-sm focus:outline-none focus:ring focus:border-indigo-500 focus:ring-indigo-200"
       type="text"
       @blur="edit = false"
     />
-    <button
-      v-if="hover && props.args.disposable"
-      class="absolute top-6 right-8"
-    >
-      <TrashIcon
-        class="h-5 fill-red-200 transition hover:fill-red-400 hover:rotate-[-4deg]"
-      />
-    </button>
+    <div class="absolute top-6 right-4 flex justify-end gap-4">
+      <button v-show="hover" @click="editPrice()">
+        <PencilSquareIcon
+          class="h-5 fill-slate-600 transition hover:fill-slate-400 hover:rotate-[-4deg]"
+        />
+      </button>
+      <button
+        v-if="hover && props.args.disposable"
+        @click="emits('delete', props.args.id)"
+      >
+        <TrashIcon
+          class="h-5 fill-red-200 transition hover:fill-red-400 hover:rotate-[-4deg]"
+        />
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { computed, nextTick, ref } from 'vue';
 import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/20/solid';
 import type { PriceComponentProps } from '@/types/price-component.type';
 
 const hover = ref(false);
 const edit = ref(false);
-// Workaround for typed props from here: https://github.com/vuejs/core/issues/4294#issuecomment-984033739
+const input = ref(null);
+
 const props = withDefaults(defineProps<{ args: PriceComponentProps }>(), {});
+const emits = defineEmits<{
+  (e: 'delete', id: string): void;
+}>();
+
 const price = ref(props.args.initialValue);
+
 const computedPrice = computed(() => {
   return parseFloat(price.value.toString()).toFixed(2);
 });
-const filterValue = () => {
-  console.log('filterValue');
+
+const editPrice = () => {
   edit.value = true;
+  nextTick(() => {
+    input.value.focus();
+  });
 };
 </script>
 
--- src/components/Containers/FormContainer.vue
+++ src/components/Containers/FormContainer.vue
@@ -5,6 +5,7 @@
       v-for="component in components"
       :key="component.id"
       :args="component"
+      @delete="deletePriceComponent"
     />
   </div>
 </template>
@@ -16,12 +17,21 @@ import { storeToRefs } from 'pinia';
 import { useStore } from '@/stores';
 
 const { components } = storeToRefs(useStore());
+
 components.value.push({
   id: '124',
   initialValue: 2.567,
   label: 'Metal Price',
   disposable: true,
 });
+
+const deletePriceComponent = (event: any) => {
+  console.log('price delete:', event);
+  components.value.splice(
+    components.value.findIndex(item => item.id === event),
+    1,
+  );
+};
 </script>
 
 <style scoped></style>

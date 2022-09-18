40c437b3c3dc9914b959473bf2cf3e29b230ee98
Mohamed Gallah <mohamed.gallah@gmail.com>
2022-09-18 21:23:46 +0200

    ♻️ Fix a weird bug in GhostPrice after adding new price. Use uuidv4 as ids for price components.

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,12 @@
 import { computed, ref } from 'vue';
 import { defineStore } from 'pinia';
+import { v4 as uuidv4 } from 'uuid';
 import type { PriceComponentProps } from '@/types/price-component.type';
 
 export const useStore = defineStore('components', () => {
   const components = ref<Array<PriceComponentProps>>([
     {
-      id: '123',
+      id: uuidv4(),
       initialValue: 1,
       label: 'Base Price',
       disposable: false,

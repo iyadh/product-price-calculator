453edf797b47317c3f6d2112c3fdb4dcef55dc6b
Mohamed Gallah <mohamed.gallah@gmail.com>
2022-09-18 18:38:48 +0200

    ✨ Add GhostPrice validation

--- src/components/Atoms/GhostPrice.vue
+++ src/components/Atoms/GhostPrice.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="relative flex justify-between px-8 py-4">
-    <input
-      v-model="label"
-      :class="{ 'text-slate-500': edit, 'text-slate-200': !edit }"
-      class="w-2/4 block rounded-md text-sm leading-5 py-2 px-3 mr-14 border-2 border-slate-100 shadow-sm focus:outline-none focus:ring focus:border-indigo-500 focus:ring-indigo-200 placeholder:text-slate-200"
-      placeholder="Label goes here .."
-      type="text"
-      @focus="edit = true"
-    />
-    <input
-      v-model="price"
-      :class="{ 'text-slate-500': edit, 'text-slate-200': !edit }"
-      class="w-1/4 block rounded-md text-sm leading-5 py-2 px-3 mr-14 border-2 border-slate-100 shadow-sm focus:outline-none focus:ring focus:border-indigo-500 focus:ring-indigo-200 placeholder:text-slate-200"
-      type="text"
-      @focus="edit = true"
-    />
+    <div class="w-2/4 flex flex-col">
+      <input
+        v-model="label"
+        :class="{ 'text-slate-500': edit, 'text-slate-200': !edit }"
+        class="w-full block rounded-md text-sm leading-5 py-2 px-3 mr-14 border-2 border-slate-100 shadow-sm focus:outline-none focus:ring focus:border-indigo-500 focus:ring-indigo-200 placeholder:text-slate-200"
+        placeholder="Label goes here .."
+        type="text"
+        @focus="edit = true"
+      />
+      <p v-if="errors.label" class="mt-2 text-xs text-red-400">
+        {{ errors.label }}
+      </p>
+    </div>
+
+    <div class="w-1/4 mr-14 flex flex-col">
+      <input
+        v-model="price"
+        :class="{ 'text-slate-500': edit, 'text-slate-200': !edit }"
+        class="w-full block rounded-md text-sm leading-5 py-2 px-3 border-2 border-slate-100 shadow-sm focus:outline-none focus:ring focus:border-indigo-500 focus:ring-indigo-200 placeholder:text-slate-200"
+        type="text"
+        @focus="edit = true"
+      />
+      <p v-if="errors.price" class="mt-2 text-xs text-red-400">
+        {{ errors.price }}
+      </p>
+    </div>
+
     <div v-if="edit" class="absolute top-6 right-4 flex justify-end gap-4">
       <button @click="addNewPriceComponent()">
         <CheckIcon
@@ -31,25 +43,45 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { CheckIcon, XMarkIcon } from '@heroicons/vue/20/solid';
+import { ref, watch } from 'vue';
 import { storeToRefs } from 'pinia';
+import { CheckIcon, XMarkIcon } from '@heroicons/vue/20/solid';
 import { useStore } from '@/stores';
+import { validateLabel, validatePrice } from '@/types/InputValidation.type';
 
 const { components } = storeToRefs(useStore());
 
 const edit = ref(false);
 const label = ref('');
 const price = ref(0);
+const errors = ref({
+  label: '',
+  price: '',
+});
+
+watch([label, price], ([newLabel, newPrice]) => {
+  errors.value.label = validateLabel(
+    newLabel === label.value ? label.value : newLabel,
+  );
+  errors.value.price = validatePrice(
+    newPrice === price.value ? price.value : newPrice,
+  );
+});
+
+const validate = () => {
+  return errors.value.label.length === 0 && errors.value.price.length === 0;
+};
 
 const addNewPriceComponent = () => {
-  components.value.push({
-    id: (components.value.length + 1).toString(),
-    label: label.value,
-    initialValue: price.value,
-    disposable: true,
-  });
-  reset();
+  if (validate()) {
+    components.value.push({
+      id: (components.value.length + 1).toString(),
+      label: label.value,
+      initialValue: parseFloat(price.value.toString().replace(',', '.')),
+      disposable: true,
+    });
+    reset();
+  }
 };
 const reset = () => {
   edit.value = false;
